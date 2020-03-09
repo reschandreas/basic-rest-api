ca50092392dd0f6316215d41983675afabbffdfd
Andreas Resch <andreas@resch.io>
2020-03-09 23:26:57 +0100

    fixed secret

--- src/main/kotlin/$tld/$name/$package/auth/SecurityConstants.kt
+++ src/main/kotlin/$tld/$name/$package/auth/SecurityConstants.kt
@@ -1,7 +1,10 @@
 package $tld.$name.$package.auth
 
+import java.math.BigInteger
+import java.util.*
+
 internal object SecurityConstants {
-    val SECRET = UUID.randomUUID().toString().substring(0,128)
+    val SECRET = String(BigInteger(128, Random()).toByteArray())
     val EXPIRATION_TIME: Long = 84600
     val TOKEN_PREFIX = "Bearer "
     val HEADER_STRING = "Authorization"
